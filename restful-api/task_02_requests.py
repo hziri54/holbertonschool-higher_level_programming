400e0dcfb8129ca940458c878c88d72a4045238a
Ziri Houamdi <zirihouamdi@MacBook-Air-de-Ziri.local>
2024-07-25 20:39:00 +0200

    task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,21 +1,30 @@
 import requests
 import csv
 
-def fetch_posts():
+def fetch_and_print_posts():
+    # Fetch response
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    return response.json() if response.status_code == 200 else None
 
-def print_posts_titles(posts):
-    for post in posts:
-        print(post['title'])
+    # Print response status
+    print(f"Status Code: {response.status_code}")
 
-def save_posts_to_csv(posts, filename='posts.csv'):
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-        writer.writeheader()
-        writer.writerows(posts)
+    # Print page title if response is OK
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
 
-posts = fetch_posts()
-if posts:
-    print_posts_titles(posts)
-    save_posts_to_csv(posts)
+def fetch_and_save_posts():
+    # Fetch response
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+    # If response is OK, save data to csv
+    if response.status_code == 200:
+        posts = response.json()
+        data_to_save = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+
+        # Save to CSV
+        with open('posts.csv', mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(data_to_save)
