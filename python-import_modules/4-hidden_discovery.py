f686b16f103e4eb78f3ada0b3e225f689515666a
Ziri Houamdi <zirihouamdi@MacBook-Air-de-Ziri.local>
2024-07-25 18:55:27 +0200

    task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
+    for name in dir(hidden_4):
+        if name[0:2] != "__":
             print(name)
