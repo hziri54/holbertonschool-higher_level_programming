8815b5be136610a1f3a563149b0bb95c1a823bb6
Ziri Houamdi <zirihouamdi@air-de-ziri.home>
2024-09-30 10:21:14 +0200

    task 3 test true or false

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Définir une classe pour l'objet qui hérite"""
+
+def is_kind_of_class(obj, a_class):
+    """Vérifier la fonction de l'objet"""
+    if isinstance(obj, a_class):
+        return True
+    return False
