cc4c5b8b6acf70f161ba3e19392ab8f90c386faf
Ziri Houamdi <zirihouamdi@air-de-ziri.home>
2024-09-30 11:47:23 +0200

    add return false

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,12 +1,8 @@
+#!/usr/bin/python3
+"""Féfinir la fonction qui est héritée"""
+
 def inherits_from(obj, a_class):
-    """Vérifie si l'objet est une instance d'une classe qui hérite (directement ou indirectement) 
-    de la classe spécifiée (sans être une instance directe).
-    
-    Args:
-        obj: L'objet à vérifier.
-        a_class: La classe à comparer.
-    
-    Returns:
-        True si l'objet est une instance d'une sous-classe de 'a_class', sinon False.
-    """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    """Regarder l'objet qui hérite de la classe"""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
