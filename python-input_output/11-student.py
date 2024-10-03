e7640dc2a70ef6c6456c6b31372f9ebef85d5303
Ziri Houamdi <zirihouamdi@MacBook-Air-de-Ziri.local>
2024-10-03 15:02:14 +0200

    task 11 finish

---
+++ python-input_output/11-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Ecrire la classe"""
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Création de la classe basée sur la task 10"""
+        for key, value in json.items():
+            setattr(self, key, value)
