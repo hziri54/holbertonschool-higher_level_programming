7345045e6d3d9aacc5287a314db534111f630685
Ziri Houamdi <zirihouamdi@air-de-ziri.home>
2024-10-10 16:15:55 +0200

    modif json in the beginning

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,19 +1,13 @@
+#!/usr/bin/python3
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class My_SubClass(BaseHTTPRequestHandler):
-    """
-    sub class
-    """
+class SimpleAPIHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        """
-        method do_get that handles GET requests
-        """
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b'Bonjour, ceci est une API simple !')
-
-# Utiliser My_SubClass au lieu de BaseHTTPRequestHandler
-httpd = HTTPServer(('', 8000), My_SubClass)
-print("Serveur en cours d'exécution sur le port 8000...")
-httpd.serve_forever()
+        # Définir la réponse pour différents endpoints
+        if self.path == '/':
+            # Page d'accueil simple
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Bonjour, ceci est une API simple !")
