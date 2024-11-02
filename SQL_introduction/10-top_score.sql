31cd52d3c0fb486264d3148b8ca60e2a6350a317
hziri54 <zirihouamdi6'@gmail.com>
2024-11-02 16:24:09 +0100

    task 10 debbug

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
---  A script that lists all records.
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+-- A script that lists all records of the table. 
+SELECT score, name FROM second_table ORDER BY score DESC;
