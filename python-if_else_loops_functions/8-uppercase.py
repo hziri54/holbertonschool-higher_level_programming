0c3ee24af43b40265bf05d5427da39215f4c2d85
hziri54 <zirihouamdi6'@gmail.com>
2024-11-25 17:43:42 +0100

    task 8 finish

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+
+        print("{}".format(i), end="")
+    print("")
