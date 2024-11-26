994fb245cd33b2e5ebeff5999919e2f69ebabcc1
hziri54 <zirihouamdi6'@gmail.com>
2024-11-26 12:53:17 +0100

    task 4 finish

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """Replace an element in a list at a specific position without modifying the original list."""
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+    
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
