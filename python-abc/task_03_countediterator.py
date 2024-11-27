2e2aabcb77a9fe5689b4c516d89a2446e4768fe5
hziri54 <zirihouamdi6'@gmail.com>
2024-11-27 16:01:12 +0100

    task 3 finish

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.counter = 0
+
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+            self.counter += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+
+    def get_count(self):
+        return self.counter
