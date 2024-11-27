7eed45897d9865ac0a46df05dd754aab3dff31df
hziri54 <zirihouamdi6'@gmail.com>
2024-11-27 10:13:49 +0100

    task 5 finish

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoader', function() {
+    const header = document.querySelector('header');
+    const updateHeader = document.getElementById('update_header');
+    updateHeader.addEventListener('click', function() {
+    });
+  });
