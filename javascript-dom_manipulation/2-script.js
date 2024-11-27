8437ee9ecc5d88387ce72eba7c1f17098241ea9c
hziri54 <zirihouamdi6'@gmail.com>
2024-11-27 09:25:47 +0100

    task 2 finish

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('#red-header').addEventListener('click', function() {
+      document.querySelector('header').classList.add('red');
+    });
+  });
