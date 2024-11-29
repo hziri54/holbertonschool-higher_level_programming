9062dc9f301db495053877d0ac26ad582310bdf0
hziri54 <zirihouamdi6'@gmail.com>
2024-11-29 20:18:21 +0100

    modif for pycodestyle

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Defines an empty class BaseGeometry."""
 
+
 class BaseGeometry:
     """Represents a base geometry."""
+
 
     def area(self):
         """Raises an exception if not implemented."""
