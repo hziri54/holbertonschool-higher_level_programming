23104bec1acc7fc828ccf3d7dacb2d137b75b071
hziri54 <zirihouamdi6'@gmail.com>
2024-11-29 21:45:09 +0100

    modif pycodestyle

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Learning Python is an exciting, fast-paced, object-oriented programming language that is widely used in various fields"
-str = str[9:15] + str[36:50] + str[82:88]
+str = "Python is an interpreted, interactive, object-oriented programing language\
+that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
