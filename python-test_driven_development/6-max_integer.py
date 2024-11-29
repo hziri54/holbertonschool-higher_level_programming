dcb37fd580adeaec46b4ec850c09db167f650789
hziri54 <zirihouamdi6'@gmail.com>
2024-11-29 23:16:35 +0100

    task 6 finish

--- python-test_driven_development/6-max_integer.py
+++ python-test_driven_development/6-max_integer.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""Module to find the max integer in a list
 """
-import unittest
-max_integer = __import__('6-max_integer').max_integer
 
-class TestMaxInteger(unittest.TestCase):
-    """unittest max_integer"""
-    def test_list(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_reverse_list(self):
-        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
-
-    def test_middle_list(self):
-        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
-
-    def test_same_list(self):
-        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
-
-    def test_negative_number(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-
-    def test_float_list(self):
-        self.assertEqual(max_integer([1.5, 2.5, 3.5, 4.5]), 4.5)
-
-    def test_one_element_list(self):
-        self.assertEqual(max_integer([1]), 1)
-
-    def test_string(self):
-        self.assertEqual(max_integer(["Zamdane"]), "Zamdane")
-
-    def test_empty_list(self):
-        self.assertIsNone(max_integer([]), None)
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
