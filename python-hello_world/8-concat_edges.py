f87ffbcd8a289b7be3feab3f6061f220dc2ce297
hziri54 <zirihouamdi6'@gmail.com>
2024-11-29 21:35:19 +0100

    modif pycodestyle

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-import sys
-print("object", end="") 
-print("-oriented", end=" ") 
-print("programming", end=" ") 
-print("with", end=" ") 
-print("Python")
+str = "object-oriented programming with Python"
+str = str[0:7] + str[7:18] + str[18:33]
+print(str)
