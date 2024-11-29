1755951926cb11938e8d4c0424f7a087c9da6ced
hziri54 <zirihouamdi6'@gmail.com>
2024-11-29 22:10:51 +0100

    add test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    if type(a) not in [int, float]:
+    """ addition of a and b"""
+    if not isinstance(a,(int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b,(int, float)):
         raise TypeError("b must be an integer")
-    
-    # Conversion des valeurs en entiers si ce sont des flottants
-    return int(a) + int(b)
+    return (int(a) + int(b))
