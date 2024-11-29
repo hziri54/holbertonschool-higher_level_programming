e6729f2b1ff5abe427cb83161a26f66ef047f169
hziri54 <zirihouamdi6'@gmail.com>
2024-11-29 21:29:51 +0100

    task 15 test checker

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
--- A script that lists all Comedy shows in the databaseg.
-SELECT tv_shows.title 
+-- script that lists all Comedy shows in the hbtn_0d_tvshows database.
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_shows_genre_id
-WHERE tv_genres.name = "Comedy"
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
