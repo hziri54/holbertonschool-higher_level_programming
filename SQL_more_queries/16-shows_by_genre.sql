6df76d1a8572cfdf9acec0463826f2781d133a40
hziri54 <zirihouamdi6'@gmail.com>
2024-11-29 21:31:28 +0100

    remove upper case for lower case

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
--- A script that lists all shows.
-SELECT tv_shows.title, tv_genres.name 
+-- script that lists all shows and their associated genres from the hbtn_0d_tvshows database.
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_shows_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
