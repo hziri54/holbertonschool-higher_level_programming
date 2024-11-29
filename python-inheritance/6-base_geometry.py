dcc5fc1087aa13b519d4beb00d83f7d5e53d5766
hziri54 <zirihouamdi6'@gmail.com>
2024-11-29 20:16:59 +0100

    task 6 finish

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
 class BaseGeometry:
+    """Represents a base geometry."""
+
     def area(self):
+        """Raises an exception if not implemented."""
         raise Exception("area() is not implemented")
